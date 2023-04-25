2fa9d69f976f8639d4238f1366bcc40ab9d2acba
Loyslene Alves Montanari <lamontanari@brasigran.com.br>
2023-04-25 15:28:46 -0300

    Adicionando campos do usuario para perfil

--- Vtreffects.Dominio/Modelo/Usuario.cs
+++ Vtreffects.Dominio/Modelo/Usuario.cs
@@ -6,6 +6,9 @@
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string Foto { get; set; }
+        public string Bio { get; set; }
+
+        public string? InstrumentoPrincipal { get; set; }
+        public string? Foto { get; set; }
     }
 }
